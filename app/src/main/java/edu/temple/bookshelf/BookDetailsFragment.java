d01ec949bc2bc60d7d0827ba3384635a388375a7
nickbonanni <nickbo514@yahoo.com>
2021-04-02 21:16:09 -0400

    Added image covers to BookDetailFragment

--- app/src/main/java/edu/temple/bookshelf/BookDetailsFragment.java
+++ app/src/main/java/edu/temple/bookshelf/BookDetailsFragment.java
@@ -1,5 +1,6 @@
 package edu.temple.bookshelf;
 
+import android.net.Uri;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
@@ -7,6 +8,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class BookDetailsFragment extends Fragment {
 
@@ -72,6 +75,7 @@ public class BookDetailsFragment extends Fragment {
         /*
         imageViewCover.setImageResource();
         */
+        Picasso.get().load(Uri.parse(book.getCoverURL())).into(imageViewCover);
 
     }
 }
